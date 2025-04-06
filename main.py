6936c362a28b348e65ef0d4398a5f0be1d1219f3
Dmitry Morozov <morozov124@gmail.com>
2025-04-07 01:38:46 +0300

    * Added progress bar * Added relays_lst and its sorting * Reorganized module to use tests * Added new startup attributes

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import argparse
 import asyncio
 import sys
 import urllib.parse
-from itertools import count
+from asyncio import sleep, CancelledError
+from itertools import count, cycle
 from typing import Any
 
 import requests
@@ -12,6 +13,7 @@ from settings import BASEURL, HEADERS, settings
 
 number = count(start=1)
 semaphore: asyncio.Semaphore = asyncio.Semaphore(settings.OPEN_FILES)
+relays_lst: list[Relay] = []
 
 
 def grab() -> requests.Response:
@@ -23,7 +25,7 @@ def grab() -> requests.Response:
         try:
             with requests.get(url, timeout=settings.TIMEOUT, headers=HEADERS) as response:
                 return response
-        except:
+        except BaseException:
             ...
     print("Didn't get a list of relays")
     sys.exit(1)
@@ -34,15 +36,42 @@ def callback(task: asyncio.Task[Any]) -> None:
             and not task.exception()):
         relay = task.result()
         if relay:
+            relays_lst.append(relay)
+
+
+def output_(
+        bandwidth: bool,
+        guard_relays: bool
+) -> None:
+    """Split for test purpose"""
+
+    print("\r         "
+          "address                          "
+          "fingerprint                "
+          "country_name   "
+          "first_seen "
+          "guard_probability advertised_bandwidth"
+          )
+
+    for relay in relays_lst:
+        if bandwidth and relay.advertised_bandwidth or guard_relays and relay.guard_probability \
+                or not (bandwidth or guard_relays):
             print(
                 f"{next(number):3d}. "
                 f"{relay.or_addresses.ip4:<21} "
                 f"{relay.fingerprint:<40} "
-                f"{relay.country_name[:14]:^15}  "
-                f"{relay.first_seen[:10]} "
-                f"{0 if relay.guard_probability is None else relay.guard_probability:13.7f}    "
-                f"{relay.advertised_bandwidth / 1049000:10.2f} MiB/s"
+                f"{relay.country_name[:14] if relay.country_name is not None else '':^15}  "
+                f"{relay.first_seen[:10] if relay.first_seen is not None else ''} "
+                f"{relay.guard_probability if relay.guard_probability is not None else 0:13.7f}    "
+                f"{relay.advertised_bandwidth / 1049000 if relay.advertised_bandwidth is not None else 0:10.2f} MiB/s"
             )
+
+
+def output() -> None:
+    relays_lst.sort(
+        key=lambda x: x.advertised_bandwidth if x.advertised_bandwidth is not None else 0, reverse=True
+    )
+    output_(args.bandwidth, args.guard_relays)
 
 
 def parse(response: requests.Response) -> list[Relay]:
@@ -54,17 +83,32 @@ def parse(response: requests.Response) -> list[Relay]:
         sys.exit(1)
 
 
+async def progress_bar() -> None:
+    hide_cursor = '\x1b[?25l'
+    show_cursor = '\x1b[?25h'
+    samples = ('⣾', '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽')
+    items = cycle(samples)
+    try:
+        while True:
+            await sleep(0.5)
+            print(f'{hide_cursor}\rAnalysis in progress {next(items)}', end='')
+    except CancelledError:
+        print(show_cursor)
+
+
 async def main(relays: list[Relay]) -> None:
     try:
+        progress = asyncio.create_task(progress_bar())
         async with asyncio.TaskGroup() as group:
             for relay in relays:
-                # In Tor metrics We Trust
-                if relay.guard_probability or args.all_relays:
-                    group.create_task(connect(relay)).add_done_callback(callback)
+                group.create_task(connect(relay)).add_done_callback(callback)
     except BaseException:
         print(
             '>>> Reduce the OPEN_FILES value in settings.py to avoid the "Too many open files" error.'
         )
+    else:
+        progress.cancel()
+        output()
 
 
 async def connect(relay: Relay) -> Relay | None:
@@ -88,20 +132,22 @@ async def connect(relay: Relay) -> Relay | None:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-a',
-        '--all',
-        dest='all_relays',
+        '-g',
+        '--guard',
+        dest='guard_relays',
         action='store_const',
         const=True,
         default=False,
-        help='display all relays'
+        help='display only relays with positive guard_probability'
+    )
+    parser.add_argument(
+        '-b',
+        '--bandwidth',
+        dest='bandwidth',
+        action='store_const',
+        const=True,
+        default=False,
+        help='display only relays with positive advertised_bandwidth'
     )
     args = parser.parse_args()
-    print("         "
-          "address                          "
-          "fingerprint                "
-          "country_name   "
-          "first_seen "
-          "guard_probability advertised_bandwidth"
-          )
     asyncio.run(main(parse(grab())))
